2b52e1da83f3874b747264d862e5996aaf676465
Parimal <parimalmeshram15@gmail.com>
2025-06-15 18:50:24 +0530

    Added Security Group and Ingress Rule Resource

---
+++ Challenge 1/modules/SG/main.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "web_app_sg" {
+  vpc_id      = var.vpc_id
+  name        = "allow_ssh_http"
+  description = "This SG allows SSH and HTTP port for the instance"
+  tags = {
+    Name = "PROD"
+  }
+}
+
+# Note:
+# - `for_each` requires a map or a set
+# - Sets don't support complex objects (like list of objects)
+# - So we convert list of objects to a map using index as key
+# - The index key is just a placeholder to satisfy `for_each`'s map requirement
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.web_app_sg.id
+
+  for_each = { for index, obj in var.ingress_rules : index => obj }
+
+  from_port   = each.value.from_port
+  to_port     = each.value.to_port
+  ip_protocol = each.value.protocol
+  cidr_ipv4   = each.value.cidr_blocks
+  description = each.value.description
+}
+
---
+++ Challenge 1/modules/SG/variable.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {}
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = string
+    description = string
+  }))
+}
